a195c032f011e10fbc3dd70df59f10ee5fbd6a09
AhmedNagii <ahmednagihassan09@gmail.com>
2025-11-01 13:51:07 +0200

    A Better File Structure

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
+const toursRouter = require('./routes/tourRoutes');
+const usersRouter = require('./routes/userRoutes');
 
 const app = express();
+
+app.use(morgan('dev'));
 
 app.use(express.json());
 
@@ -12,129 +15,9 @@ app.use((req, res, next) => {
   next();
 });
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
-);
+app.use('/api/v1/tours', toursRouter);
+app.use('/api/v1/users', usersRouter);
 
-const getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    results: tours.length,
-    data: { tours },
-  });
-};
 
-const createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: newId }, req.body);
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).json({
-        status: 'success',
-        data: { tour: newTour },
-      });
-    }
-  );
-};
 
-const getTour = (req, res) => {
-  // convert the string to a number
-  const id = +req.params.id;
-  const tour = tours.find((el) => el.id === id);
-
-  if (Number.isNaN(id)) {
-    return res.status(400).json({ status: 'fail', message: 'Invalid id' });
-  }
-
-  if (!tour) {
-    return res.status(404).json({ status: 'fail', message: 'Tour not found' });
-  }
-  res.status(200).json({
-    status: 'success',
-    data: { tour },
-  });
-};
-
-const updateTour = (req, res) => {
-  // convert the string to a number
-  const id = +req.params.id;
-  if (Number.isNaN(id)) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Invalid id',
-    });
-  }
-
-  const tourIndex = tours.findIndex((el) => el.id === id);
-
-  if (tourIndex === -1) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Tour not found',
-    });
-  }
-
-  tours[tourIndex] = { ...tours[tourIndex], ...req.body.updatedValues };
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      if (err) {
-        return res.status(500).json({
-          status: 'error',
-          message: 'Error writing to file',
-        });
-      }
-      res.status(200).json({
-        status: 'success',
-        data: { tour: tours[tourIndex] },
-      });
-    }
-  );
-};
-
-const deleteTour = (req, res) => {
-  // convert the string to a number
-  const id = +req.params.id;
-  if (Number.isNaN(id)) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Invalid id',
-    });
-  }
-
-  const updatedTours = tours.filter((el) => el.id !== id);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(updatedTours),
-    (err) => {
-      if (err) {
-        return res.status(500).json({
-          status: 'error',
-          message: 'Error writing to file',
-        });
-      }
-      res.status(204).json({
-        status: 'success',
-        data: null,
-      });
-    }
-  );
-};
-
- app.route('/api/v1/tours').get(getAllTours).post(createTour);
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
-
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+module.exports = app;
---
+++ controllers/tourController.js
@@ -0,0 +1,116 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, 'utf-8')
+);
+
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
+  });
+};
+
+exports.createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: { tour: newTour },
+      });
+    }
+  );
+};
+
+exports.getTour = (req, res) => {
+  // convert the string to a number
+  const id = +req.params.id;
+  const tour = tours.find((el) => el.id === id);
+
+  if (Number.isNaN(id)) {
+    return res.status(400).json({ status: 'fail', message: 'Invalid id' });
+  }
+
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'Tour not found' });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: { tour },
+  });
+};
+
+exports.updateTour = (req, res) => {
+  // convert the string to a number
+  const id = +req.params.id;
+  if (Number.isNaN(id)) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Invalid id',
+    });
+  }
+
+  const tourIndex = tours.findIndex((el) => el.id === id);
+
+  if (tourIndex === -1) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Tour not found',
+    });
+  }
+
+  tours[tourIndex] = { ...tours[tourIndex], ...req.body.updatedValues };
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        return res.status(500).json({
+          status: 'error',
+          message: 'Error writing to file',
+        });
+      }
+      res.status(200).json({
+        status: 'success',
+        data: { tour: tours[tourIndex] },
+      });
+    }
+  );
+};
+
+exports.deleteTour = (req, res) => {
+  // convert the string to a number
+  const id = +req.params.id;
+  if (Number.isNaN(id)) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Invalid id',
+    });
+  }
+
+  const updatedTours = tours.filter((el) => el.id !== id);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(updatedTours),
+    (err) => {
+      if (err) {
+        return res.status(500).json({
+          status: 'error',
+          message: 'Error writing to file',
+        });
+      }
+      res.status(204).json({
+        status: 'success',
+        data: null,
+      });
+    }
+  );
+};
